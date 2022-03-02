d485207b9f0640587e041e6a8334e64f73b941a9
Márcio Dias <eu@marciodias.me>
2022-03-02 14:25:09 -0300

    FIx

--- src/services/BaileysService.ts
+++ src/services/BaileysService.ts
@@ -1,34 +1,34 @@
-import makeWASocket, { DisconnectReason } from "@adiwajshing/baileys";
-import { Boom } from "@hapi/boom";
+// import makeWASocket, { DisconnectReason } from "@adiwajshing/baileys";
+// import { Boom } from "@hapi/boom";
 
-class BaileysService {
-    async function connectToWhatsApp () {
-        const sock = makeWASocket({
-            // can provide additional config here
-            printQRInTerminal: true
-        });
+// class BaileysService {
+//     async function connectToWhatsApp () {
+//         const sock = makeWASocket({
+//             // can provide additional config here
+//             printQRInTerminal: true
+//         });
 
-        sock.ev.on('connection.update', (update) => {
-            const { connection, lastDisconnect } = update
-            if(connection === 'close') {
-                const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
-                console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
-                // reconnect if not logged out
-                if(shouldReconnect) {
-                    connectToWhatsApp()
-                }
-            } else if(connection === 'open') {
-                console.log('opened connection')
-            }
-        });
-        
-        sock.ev.on('messages.upsert', m => {
-            console.log(JSON.stringify(m, undefined, 2))
+//         sock.ev.on('connection.update', (update) => {
+//             const { connection, lastDisconnect } = update
+//             if(connection === 'close') {
+//                 const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
+//                 console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+//                 // reconnect if not logged out
+//                 if(shouldReconnect) {
+//                     connectToWhatsApp()
+//                 }
+//             } else if(connection === 'open') {
+//                 console.log('opened connection')
+//             }
+//         });
 
-            console.log('replying to', m.messages[0].key.remoteJid)
-            await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'Hello there!' })
-        });
-    }
-}
+//         sock.ev.on('messages.upsert', m => {
+//             console.log(JSON.stringify(m, undefined, 2))
 
-export { BaileysService };
+//             console.log('replying to', m.messages[0].key.remoteJid)
+//             await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'Hello there!' })
+//         });
+//     }
+// }
+
+// export { BaileysService };
--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -1,21 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import { RateLimiterRedis } from "rate-limiter-flexible";
-// import redis from "redis";
+import redis from "redis";
 
 import { RateLimiterError } from "@shared/errors/RateLimiterError";
 
-// const redisClient = redis.createClient({
-//   host: process.env.REDIS_HOST,
-//   port: Number(process.env.REDIS_PORT),
-//   enable_offline_queue: false,
-// });
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  enable_offline_queue: false,
+});
 
-// const limiter = new RateLimiterRedis({
-//   storeClient: redisClient,
-//   keyPrefix: "rateLimiter",
-//   points: 10, // 10 requests
-//   duration: 1, // por 1 second, by IP
-// });
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "rateLimiter",
+  points: 10, // 10 requests
+  duration: 1, // por 1 second, by IP
+});
 
 export default async function rateLimiter(
   request: Request,
@@ -23,7 +23,7 @@ export default async function rateLimiter(
   next: NextFunction
 ): Promise<void> {
   try {
-    // await limiter.consume(request.ip);
+    await limiter.consume(request.ip);
 
     next();
   } catch (error) {
