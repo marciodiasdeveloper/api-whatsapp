973d9a8d48215e4f2c0037bc001824422e06650d
Márcio Dias <eu@marciodias.me>
2022-03-12 19:14:15 -0300

    Create Test Files

---
+++ src/modules/whatsapp/useCases/checkNumberStatus/checkNumberStatusUseCase.spec.ts
@@ -0,0 +1,10 @@
+describe("Add Participant Controller", () => {
+  beforeEach(() => {
+    // checkEmailExistsUseCase = new CheckEmailExistsUseCase({
+    //   email,
+    // });
+    // createUserUseCase = new CheckEmailExistsUseCase(UsersRepositoryInMemory);
+  });
+
+  it("Should initial test example", async () => {}, 50000);
+});
