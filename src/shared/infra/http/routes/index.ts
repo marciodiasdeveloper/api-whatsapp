ed775a178b882463a1c9689c3065adc99e8431cf
Márcio Dias <eu@marciodias.me>
2022-03-12 18:51:47 -0300

    FIx

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -7,15 +7,24 @@ const router = Router();
 const startController = new StartController();
 
 router.get("/start", startController.handle);
-// router.get('/qrcode', QrCodeController.handle);
-// router.get('/status', StatusController.handle);
-// router.get('/device', DeviceController.handle);
-// router.get('/sendText', SendTextController.handle);
-// router.get('/sendFile', SendFileController.handle);
-// router.get('/close', CloseController.handle);
-// router.get('/groups', GroupsController.handle);
-// router.get('/checkPhone', CheckPhoneController.handle);
-// router.get('/battery', BatteryController.handle);
+// router.get('/', QrCodeController.handle);
+// router.get('/', StatusController.handle);
+// router.get('/', DeviceController.handle);
+// router.get('/', SendTextController.handle);
+// router.get('/', SendFileController.handle);
+// router.get('/', CloseController.handle);
+// router.get('/', GroupsController.handle);
+// router.get('/', CheckPhoneController.handle);
+// router.get('/', BatteryController.handle);
+// router.get('/', QrCodeController.handle);
+// router.get('/', StatusController.handle);
+// router.get('/', DeviceController.handle);
+// router.get('/', SendTextController.handle);
+// router.get('/', SendFileController.handle);
+// router.get('/', CloseController.handle);
+// router.get('/', GroupsController.handle);
+// router.get('/', CheckPhoneController.handle);
+// router.get('/', BatteryController.handle);
 
 router.get("/", (request: Request, response: Response) => {
   return response.json({ success: true, api: "api-discord" });
